8e41d020d1e0511e2742a816a4e2d442c245d3d1
Romain Muller <romain.muller@esial.net>
2010-09-23 18:57:36 +0200

    Initial import of Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Godwin::Application.config.secret_token = 'cbacf2a9ea7a8f478e938fd27a538b066df1782dfb8eace8571679ee2fe73aefa511a3b62d46cf014f4f5e16041ca54201ca88dbfd6a650ee2a22ffa7be0e632'
