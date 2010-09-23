ccea3ababc7998064652b07bac39121ec46fa06f
Romain Muller <romain.muller@esial.net>
2010-09-23 19:45:05 +0200

    HTML5 reset in HAML

---
+++ app/controllers/godwin_controller.rb
@@ -0,0 +1,5 @@
+class GodwinController < ApplicationController
+  def index
+    # The view will do the work.
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def conditional_body(&block)
+    raw %Q{<!--[if lt IE 7 ]><body class='ie6'><![endif]-->
+      <!--[if IE 7 ]><body class='ie7'><![endif]-->
+      <!--[if IE 8 ]><body class='ie8'><![endif]-->
+      <!--[if IE 9 ]><body class='ie9'><![endif]-->
+      <!--[if (gt IE 9)|!(IE)]><!--><body><!--<![endif]-->
+      #{capture(&block)}
+      </body>}
+  end
 end
